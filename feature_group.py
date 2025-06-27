18884f5a861084ea08fabbc96584f56dab8827fa
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2025-06-27 23:03:15 +0500

    Create feature_group.py

---
+++ feature_group.py
@@ -0,0 +1,23 @@
+import hopsworks
+import pandas as pd
+import os
+
+# Step 1: Login using API key
+api_key = os.getenv("RI6aVh8JRlgiuVaz.bGgoZw1u0Lf54YkBoZyivKakNFHWMHcQE3z5hCk4GOpTbHKf7jHLol2cXmSfZSMC")
+project = hopsworks.login(api_key_value=api_key)
+fs = project.get_feature_store()
+
+# Step 2: Read full daily CSV
+df = pd.read_csv("lahore_aqi_features.csv")
+
+# Step 3: Create or get feature group
+fg = fs.get_or_create_feature_group(
+    name="lahore_aqi_group",
+    version=1,
+    primary_key=["timestamp"],
+    description="Lahore AQI daily updated dataset"
+    
+)
+
+# Step 4: Overwrite data
+fg.insert(df, write_options={"wait_for_job": True, "overwrite": True})
