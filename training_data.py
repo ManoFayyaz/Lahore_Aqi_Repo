23e1a743b70b27c5340a63d323b68aa06635a64a
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2025-06-27 23:55:40 +0500

    Update training_data.py

--- training_data.py
+++ training_data.py
@@ -1,9 +1,8 @@
 import hopsworks
 import pandas as pd
 import os
-
-
-project=hopsworks.login(api_key_value="secrets.HOPSWORKS_API_KEY")
+api_key = os.getenv("HOPSWORKS_API_KEY")
+project = hopsworks.login(api_key_value=api_key)
 fs=project.get_feature_store()
 
 feature_view = fs.get_feature_view(name="lahore_aqi_view", version=1)
