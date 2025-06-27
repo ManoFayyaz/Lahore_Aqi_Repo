d5e16e812ff577a0797e5d6012e97be4f84ae4ea
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2025-06-27 23:03:46 +0500

    Create feature_view.py

---
+++ feature_view.py
@@ -0,0 +1,22 @@
+import hopsworks
+import os
+
+api_key = os.getenv("RI6aVh8JRlgiuVaz.bGgoZw1u0Lf54YkBoZyivKakNFHWMHcQE3z5hCk4GOpTbHKf7jHLol2cXmSfZSMC")
+project = hopsworks.login(api_key_value=api_key)
+fs = project.get_feature_store()
+
+# Get the feature group
+fg = fs.get_feature_group("lahore_aqi_group", version=1)
+
+# Create the feature view (only once)
+fv = fs.get_or_create_feature_view(
+    name="lahore_aqi_view",
+    version=1,
+    description="Feature view for AQI prediction",
+    labels=["aqius"],  # Target column
+    query=fg.select([
+        "timestamp", "temperature", "humidity", "wind_speed", "main_pollutant", "aqius"
+    ])
+)
+
+print("✅ Feature view created.")
